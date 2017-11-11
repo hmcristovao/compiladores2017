3c377c27561e0ff489e5c2ce79ba4c0628c5df63
Andrey <Andrey@192.168.1.5>
2017-11-11 17:01:55 -0200

    CONFIRMANDO

--- Analizador Léxico/src/semantica/Simbolo.java
+++ Analizador Léxico/src/semantica/Simbolo.java
@@ -4,11 +4,11 @@ public class Simbolo {
     private String nome;      // nome da vari�vel
     private int referencia;   // refer�ncia usada na gera��o do c�digo destino
     private static int marcador = 1; // armazena a �ltima refer�ncia inclu�da na tabela
-    private String tipo;
     
-    public Simbolo(String _nome, String _tipo) {
+    
+    public Simbolo(String _nome) {
           this.nome = _nome;
-          this.tipo = _tipo;
+          //this.tipo = _tipo;
           this.referencia = Simbolo.marcador;
           Simbolo.marcador += 2;         
     }
@@ -18,10 +18,10 @@ public class Simbolo {
     public int getReferencia() {
           return this.referencia;
     }
-    public String getTipo() {
-		return this.tipo;
-	}
+   // public String getTipo() {
+	//	return this.tipo;
+	//}
     public String toString() {
-          return "Tipo:"+this.getTipo() + " / " + "Nome:"+this.getNome() + " / " + "Refer�ncia:"+this.getReferencia() + "\n";
+          return "Nome:"+this.getNome() + " / " + "Refer�ncia:"+this.getReferencia() + "\n";
     }
 }
