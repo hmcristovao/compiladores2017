4317b2163ffcd335600b8f99b9f66f6f2d863617
Andrey <acontadini@gmail.com>
2017-11-14 12:28:01 -0200

    (incompleto) Classe para comando Teste

---
+++ Analizador Léxico/src/intermediario/Condicional.java
@@ -0,0 +1,27 @@
+package intermediario;
+
+public class Condicional {
+	
+	
+	private Expressao expressao;
+	private ListaComandos listaComandos;
+
+	
+	
+	public Expressao getExpressao() {
+		return expressao;
+	}
+	
+	public void setExpressao(Expressao expressao) {
+		this.expressao = expressao;
+	}
+	
+	public ListaComandos getListaComandos() {
+		return listaComandos;
+	}
+	
+	public void setListaComandos(ListaComandos listaComandos) {
+		this.listaComandos = listaComandos;
+	}
+	
+}
---
+++ Analizador Léxico/src/intermediario/Teste.java
@@ -0,0 +1,57 @@
+package intermediario;
+
+import java.util.LinkedList;
+
+public class Teste extends Comando {
+
+	private LinkedList<Condicional> condicoes;
+	private ListaComandos outroCaso;
+	
+	
+	
+	public Teste(LinkedList<Condicional> condicoes, ListaComandos outroCaso) {
+		super();
+		this.condicoes = condicoes;
+		this.outroCaso = outroCaso;
+	}
+
+	public LinkedList<Condicional> getCondicoes() {
+		return condicoes;
+	}
+
+	public void setCondicoes(LinkedList<Condicional> condicoes) {
+		this.condicoes = condicoes;
+	}
+	
+	public void addCondicoes(Condicional condicao) {
+		this.condicoes.add(condicao);
+	}
+
+	public ListaComandos getOutroCaso() {
+		return outroCaso;
+	}
+
+	public void setOutroCaso(ListaComandos outroCaso) {
+		this.outroCaso = outroCaso;
+	}
+	
+	
+	public void saida(){
+		
+	};
+	
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		for(Condicional condicao:condicoes){	
+			condicao.toString();	
+		}
+		outroCaso.toString();
+		return null;
+	}
+
+}
