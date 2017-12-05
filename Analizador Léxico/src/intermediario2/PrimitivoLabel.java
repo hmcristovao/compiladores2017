5e3cafeb4e3f09788fcbc743d332560f497cc130
waillant <waillant@DESKTOP-JHR7KEB>
2017-12-05 15:43:31 -0200

    Primitivo Label

--- Analizador Léxico/src/intermediario2/PrimitivoLabel.java
+++ Analizador Léxico/src/intermediario2/PrimitivoLabel.java
@@ -1,19 +1,30 @@
 package intermediario2;
 
-public class PrimitivoLabel  extends PrimitivoComando {
-	private String label;
-	
-	// ...
-	
-	public String geraCodigoDestino() {
-		// gerar o código destino em assembler (JVM do Java - ver material no AVA)
-		String codigoFinal = null;
-		// ...
-		return codigoFinal;
-	}
-	
-	public String toString() {
-		// retornar a concatenação dos atributos da classe, usando toString() para cada um
-		return "";
-	}
+	public class PrimitivoLabel  extends PrimitivoComando {
+		static int contador = 0;
+		private String label;
+
+		public String geraCodigoDestino() {
+			String codigoFinal = null;
+
+			return codigoFinal = this.label;
+		}
+
+		public PrimitivoLabel(String label) {
+			super();
+			PrimitivoLabel.contador++;
+			this.label = label + "_"+ PrimitivoLabel.contador;
+		}
+
+		public String getLabel() {
+			return label;
+		}
+
+		public void setLabel(String label) {
+			this.label = label;
+		}
+
+		public String toString() {
+			return "\n\nPrimitivoComando PrimitivoLabel: " + this.label;
+		}
 }
