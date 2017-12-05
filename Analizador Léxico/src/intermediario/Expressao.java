f3572803cee72bd1619cc9ea001fc54b224ff6bb
André Luiz Brandão <andre-luiz-brandao@hotmail.com>
2017-12-05 14:00:00 -0200

    Implementação da geração de codigo destino das expressões.

--- Analizador Léxico/src/intermediario/Expressao.java
+++ Analizador Léxico/src/intermediario/Expressao.java
@@ -7,14 +7,12 @@ import semantico.*;
 
 public class Expressao 
 {
-	// atributos tais como: lista de itens, linha, expressao infixa etc
+	// atributos tais como: lista de itens, expressao infixa etc
 	private LinkedList <Item> listaInfixo;
 	private LinkedList <Item> listaPosfixo;
 	private static final long serialVersionUID = 1L;
 	private LinkedList <Item> listaExpressao;
-	private int linha;
-	private int tamPilha;
-	private static int maxPilha;
+	private int maxPilha;
 	private Tipo tipoDados;
 	private static Integer contLabel = 0;
 	private static Integer contAnd = 1;
@@ -34,29 +32,13 @@ public class Expressao
 	public void setListaExpressao(LinkedList<Item> listaExpressao) {
 		this.listaExpressao = listaExpressao;
 	}
-
-	public int getLinha() {
-		return linha;
+	
+	public int getMaxPilha() {
+		return this.maxPilha;
 	}
 
-	public void setLinha(int linha) {
-		this.linha = linha;
-	}
-
-	public int getTamPilha() {
-		return tamPilha;
-	}
-
-	public void setTamPilha(int tamPilha) {
-		this.tamPilha = tamPilha;
-	}
-
-	public static int getMaxPilha() {
-		return maxPilha;
-	}
-
-	public static void setMaxPilha(int maxPilha) {
-		Expressao.maxPilha = maxPilha;
+	public void setMaxPilha(int maxPilha) {
+		this.maxPilha = maxPilha;
 	}
 
 	public Tipo getTipoDados() {
@@ -157,7 +139,7 @@ public class Expressao
 						codigoExpressao+="dadd\r\n";
 					} else if(this.getTipoDados() == Tipo.CTE_STRING){
 
-						codigoExpressao += "astore_"+(Simbolo.getReferencia()+1) +" \r\n";
+						codigoExpressao += "astore_"+(Simbolo.getMarcador()+1) +" \r\n";
 						codigoExpressao += "astore_"+(Simbolo.getMarcador()) +" \r\n";
 						codigoExpressao += "new java/lang/StringBuilder \r\n";
 						codigoExpressao += "dup \r\n";
@@ -170,6 +152,9 @@ public class Expressao
 
 					}
 				}
+				else if(item.getValor().equals("+")){
+					codigoExpressao+="dadd\r\n";
+				}
 				else if(item.getValor().equals("-")){
 					codigoExpressao+="dsub\r\n";
 				}
@@ -179,115 +164,7 @@ public class Expressao
 				else if(item.getValor().equals("/")){
 					codigoExpressao+="ddiv\r\n";
 				}
-				else if(item.getValor().equals("^")){
-					codigoExpressao+="invokestatic    java/lang/Math.pow(DD)D\r\n";
-				}
-				else if(item.getValor().equals("e")){
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "dcmpg\r\n";
-					// Verifico se o topo é igual à 1 (ou -1)
-					codigoExpressao += "ifne topoUm_"+contAnd+"\r\n";
-
-					// Se o topo da pilha for zero, logo o resultado será sempre 0, já que
-					// 0 * (0 | 1) é sempre zero. Retiramos então o topo da pilha, empilhamos 0
-					// e partimos para a label final.
-					codigoExpressao += "pop2\r\n";
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "goto final_"+contAnd+"\r\n";
-
-					// Caso o topo seja igual à 1, entra aqui. Empilho 1 e comparo o topo com o
-					// próximo, a fim de verificar se são iguais ou não.
-					codigoExpressao += "topoUm_"+contAnd+":\r\n";
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "dcmpg\r\n";
-					// Caso seja igual à um, a resposta será um e desvia para a label 1. Senão, a operação
-					// de and irá dar zero, portanto empilharemos 0.
-					codigoExpressao += "ifne um_"+contAnd+"\r\n";
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "goto final_"+contAnd+"\r\n";
-					// Agora, tendo desempilhado 1, segue para a label 'um' e o empilhamos 1, já que 1x1 == 1.
-					codigoExpressao += "um_"+contAnd+":\r\n";
-					codigoExpressao += "dconst_1\r\n";
-					codigoExpressao += "final_"+contAnd+":\r\n";
-					contAnd++;
-				}
-				else if(item.getValor().equals("AND2")){
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "dcmpg\r\n";
-					codigoExpressao += "ifne areaDoUm_"+contAnd2+"\r\n";
-					
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "dcmpg\r\n";
-					codigoExpressao += "ifne desvioinutil_"+contAnd2+"\r\n";
-					
-					codigoExpressao += "desvioinutil_"+contAnd2+":\r\n";
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "goto finalAnd2_"+contAnd2+"\r\n";
-
-					codigoExpressao += "areaDoUm_"+contAnd2+":\r\n";
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "dcmpg\r\n";
-					codigoExpressao += "ifne verdadeiro_"+contAnd2+"\r\n";
-					
-					codigoExpressao += "dconst_0\r\n";
-					codigoExpressao += "goto finalAnd2_"+contAnd2+"\r\n";
-					
-					codigoExpressao += "verdadeiro_"+contAnd2+":\r\n";
-					codigoExpressao += "dconst_1\r\n";
-					
-					codigoExpressao += "finalAnd2_"+contAnd2+":\r\n";
-					contAnd2++;
-					
-				}
-				else if(item.getValor().equals("ou")){
-					//É necessário empilhar zero para utilizar o dcmpg e testar se o topo é zero
-					codigoExpressao+="dconst_0\r\n";
-					codigoExpressao+="dcmpg\r\n";
-					//verifica se o topo é igual a 0
-					codigoExpressao+="ifeq topoZero_"+contOr+"\r\n";
-
-					//Entra aqui se o topo for 1. 1 ou qualquer coisa = 1
-					//retira o topo da pilha
-					codigoExpressao+="pop2\r\n";
-					//empilha 1
-					codigoExpressao+="dconst_1\r\n";
-					//vai para o label final
-					codigoExpressao+="goto finalOu_"+contOr+"\r\n";
-
-					//Entra aqui se o topo for 0. O resultado de ifeq tem que empilhar 0
-					codigoExpressao+="topoZero_"+contOr+":\r\n";
-					//empilha zero para poder utilizar o dcmpg
-					codigoExpressao+="dconst_0\r\n";
-					//compara o topo e o proximo
-					codigoExpressao+="dcmpg\r\n";
-					//ser for zero vai pro label zero
-					codigoExpressao+="ifeq zero_"+contOr+"\r\n";
-					//entra aqui se o resultado nao for zero. empilha 1 pois 0 ou 1 = 1
-					codigoExpressao+="dconst_1\r\n";
-					//vai pro final
-					codigoExpressao+="goto finalOu_"+contOr+"\r\n";
-					//label zero
-					codigoExpressao+="zero_"+contOr+":\r\n";
-					//empilha 0 pois 0 ou 0 = 0
-					codigoExpressao+="dconst_0\r\n";
-
-					//label final
-					codigoExpressao+="finalOu_"+contOr+":\r\n";
-					contOr++;
-				}
-				else if(item.getValor().equals("nao")){
-					codigoExpressao+="dconst_0\r\n";
-					codigoExpressao+="dcmpg\r\n";
-					codigoExpressao+="ifeq LABEL_0"+contLabel+ "\r\n";
-					//se for diferente de zero a reposta é zero
-					codigoExpressao+="dconst_0\r\n";
-					codigoExpressao+="goto LABEL_0"+(contLabel+1)+ "\r\n";
-					//se for igual a zero a resposta é 1
-					codigoExpressao+="LABEL_0"+contLabel+ ":\r\n";
-					codigoExpressao+="dconst_1\r\n";
-					codigoExpressao+="LABEL_0"+(contLabel+1)+":\r\n";
-					contLabel+=2;
-				}
+				
 				else if(item.getValor().equals("==")){
 
 					codigoExpressao+="dcmpg\r\n";
@@ -301,53 +178,8 @@ public class Expressao
 					codigoExpressao+="LABEL_0"+(contLabel+1) + ":\r\n";
 					contLabel+=2;
 
-
 				}
-				else if(item.getValor().equals("!=")){
-					codigoExpressao+="dcmpg\r\n";
-					//Caso os numeros sejam diferentes armazena 1 na pilha.
-					codigoExpressao+="ifeq LABEL_0"+contLabel+ "\r\n";
-					codigoExpressao+="dconst_1\r\n";
-					codigoExpressao+="goto LABEL_0"+(contLabel+1) +"\r\n";
-					codigoExpressao+= ("LABEL_0"+contLabel + ":\r\n");
-					codigoExpressao+="dconst_0\r\n";
-					codigoExpressao+="LABEL_0"+(contLabel+1) + ":\r\n";
-					contLabel+=2;
-
-				}
-				else if(item.getValor().equals(">")){
-					codigoExpressao+="dcmpg\r\n"; //True: Push 1; False: Push 0 or -1;
-					//Caso o primeiro numero seja maior, armazena 1 na pilha.
-					codigoExpressao+="ifgt LABEL_0"+contLabel+ "\r\n";
-					codigoExpressao+="dconst_0\r\n";
-					codigoExpressao+="goto LABEL_0"+(contLabel+1) +"\r\n";
-					codigoExpressao+="LABEL_0"+contLabel+ ":\r\n";
-					codigoExpressao+="dconst_1\r\n";
-					codigoExpressao+="LABEL_0"+(contLabel+1) + ":\r\n";
-					contLabel += 2;
-				}
-				else if(item.getValor().equals(">=")){
-					codigoExpressao+="dcmpg\r\n"; //True: Push 1 or 0; False: Push -1;
-					//Caso o primeiro numero seja maior ou igual, armazena 1 na pilha.
-					codigoExpressao+="ifge LABEL_0"+contLabel+ "\r\n";
-					codigoExpressao+="dconst_0\r\n";
-					codigoExpressao+="goto LABEL_0"+(contLabel+1) +"\r\n";
-					codigoExpressao+="LABEL_0"+contLabel+ ":\r\n";
-					codigoExpressao+="dconst_1\r\n";
-					codigoExpressao+="LABEL_0"+(contLabel+1) + ":\r\n";
-					contLabel += 2;
-				}
-				else if(item.getValor().equals("<")){
-					codigoExpressao+="dcmpg\r\n";
-					//Caso o primeiro numero seja menor, armazena 1 na pilha.
-					codigoExpressao+="ifge LABEL_0"+contLabel+ "\r\n";
-					codigoExpressao+="dconst_1\r\n";
-					codigoExpressao+="goto LABEL_0"+(contLabel+1) +"\r\n";
-					codigoExpressao+="LABEL_0"+contLabel + ":\r\n";
-					codigoExpressao+="dconst_0\r\n";
-					codigoExpressao+="LABEL_0"+(contLabel+1) + ":\r\n";
-					contLabel += 2;
-				}
+				
 				else if(item.getValor().equals("<=")){
 					codigoExpressao+="dcmpg\r\n";
 					//Caso o primeiro numero seja menor ou igual, armazena 1 na pilha.
@@ -358,11 +190,6 @@ public class Expressao
 					codigoExpressao+="dconst_0\r\n";
 					codigoExpressao+="LABEL_0"+(contLabel+1) + ":\r\n";
 					contLabel += 2;
-				}
-				
-				//questao 3 p2
-				else if(item.getValor().equals("R")){
-					codigoExpressao+="invokestatic java/lang/Math.sqrt(D)D\r\n";
 				}
 			}
 			else if(item.getTipo() == Tipo.CTE_STRING){
