01135b629bf03bb9a04ebbfc6b6d359fdba06078
waillant <waillant@DESKTOP-JHR7KEB>
2017-12-14 13:49:47 -0200

    caso teste

--- Analizador Léxico/src/apoio/Config.java
+++ Analizador Léxico/src/apoio/Config.java
@@ -3,7 +3,7 @@ package apoio;
 public interface Config {
 	
 	 //String nomeArquivoFonte   = "teste_simples17.lek"; 
-	 String nomeArquivoFonte   = "teste.lek"; 
+	 String nomeArquivoFonte   = "teste_tarefa.lek"; 
 	 //String nomeArquivoFonte   = "prog_fonte01.lek";
 	 
 	 String nomeCodigoDestino   = "teste";
